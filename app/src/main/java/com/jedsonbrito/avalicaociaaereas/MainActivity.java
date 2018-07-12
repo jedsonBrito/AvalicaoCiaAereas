39c56ed600c47e86041b77c30fc734502ef9ce43
jedsonbrito <jedsoncid@gmail.com>
2018-07-12 10:01:36 -0300

    Tela de Splash incluida

--- app/src/main/java/com/jedsonbrito/avalicaociaaereas/MainActivity.java
+++ app/src/main/java/com/jedsonbrito/avalicaociaaereas/MainActivity.java
@@ -1,13 +1,31 @@
 package com.jedsonbrito.avalicaociaaereas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTheme(R.style.AppTheme);
+
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+
     }
+
+
+
+
+
+
+
 }
+
+
